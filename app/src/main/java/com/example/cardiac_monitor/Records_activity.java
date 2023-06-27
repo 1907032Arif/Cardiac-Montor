7f6f1eb74a8b2f7bb55ac4931b7a4045bfae8cf8
Borhan <63269814+ularif@users.noreply.github.com>
2023-06-27 11:35:15 +0600

    Activity home UI design

--- app/src/main/java/com/example/cardiac_monitor/Records_activity.java
+++ app/src/main/java/com/example/cardiac_monitor/Records_activity.java
@@ -18,36 +18,5 @@ import com.example.cardiac_monitor.databinding.ActivityRecordsBinding;
 
 public class Records_activity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityRecordsBinding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityRecordsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_records);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_records);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
