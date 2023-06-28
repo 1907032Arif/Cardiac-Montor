b1e17daa4b814a8ab34148e5d732401148f94fb2
Borhan <63269814+ularif@users.noreply.github.com>
2023-06-28 09:57:15 +0600

    Main Activity Functionality

--- app/src/main/java/com/example/cardiac_monitor/MainActivity.java
+++ app/src/main/java/com/example/cardiac_monitor/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cardiac_monitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> MainActivity.super.onBackPressed())
+                .setNegativeButton("No", null)
+                .show();
     }
 }
