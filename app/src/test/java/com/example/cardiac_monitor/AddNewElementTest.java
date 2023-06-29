1abc48e88c0637bd883fd429385e37f19f91bff3
Borhan <63269814+ularif@users.noreply.github.com>
2023-06-29 12:16:19 +0600

    Add Data Unit test

---
+++ app/src/test/java/com/example/cardiac_monitor/AddNewElementTest.java
@@ -0,0 +1,39 @@
+package com.example.cardiac_monitor;
+
+import org.junit.Test;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import com.example.cardiac_monitor.RecordList;
+
+
+
+public class AddNewElementTest {
+
+    @Test
+    public void testAddData()
+    {
+        Record record1 = new Record("80", "120", "normal", "100", "normal", "29/06/23", "11 am", "Good");
+        RecordList recordList = new RecordList();
+        recordList.addRecords(record1);
+        assertEquals(1, recordList.getRecords().size());
+
+        Record record2 = new Record("85", "130", "normal", "100", "normal", "29/06/23", "12 am", "Good");
+        recordList.addRecords(record2);
+        assertEquals(2, recordList.getRecords().size());
+
+
+        assertTrue(recordList.getRecords().contains(record1));
+        assertTrue(recordList.getRecords().contains(record2));
+
+    }
+
+
+
+
+
+}
