e2f608719f8e6e530a7c1c163a26156154a5194c
Borhan <63269814+ularif@users.noreply.github.com>
2023-07-07 23:14:14 +0600

    travis added

--- app/src/test/java/com/example/cardiac_monitor/AddNewElementTest.java
+++ app/src/test/java/com/example/cardiac_monitor/AddNewElementTest.java
@@ -22,6 +22,20 @@ public class AddNewElementTest {
      * It creates two records and adds them to the RecordList.
      * Then it checks if the RecordList contains the added records.
      */
+
+    private RecordList mockRecordList()
+    {
+        RecordList recordList = new RecordList();
+        recordList.addRecords(mockRecord());
+        return recordList;
+    }
+
+
+    private Record mockRecord()
+    {
+        return new Record("120","80","normal","60","normal","Thursday,7 July 2022","05:01 pm","sitting");
+    }
+
     @Test
     public void testAddData()
     {
@@ -104,6 +118,40 @@ public class AddNewElementTest {
         assertThrows(IllegalArgumentException.class, ()->recordList.delete(record1));
 
     }
+
+
+    @Test
+    public void testGetRecords()
+    {
+        RecordList recordList = mockRecordList();
+
+        assertEquals(0,mockRecord().compareTo(recordList.getRecords().get(0)));
+
+        Record record1 = new Record("190","60","high","50","exceptional","Thursday,7 July 2022","05:49 pm","resting");
+        recordList.addRecords(record1);
+        Record record2 = new Record("90","60","high","50","exceptional","Thursday,7 July 2022","05:49 pm","resting");
+        recordList.addRecords(record2);
+
+
+        assertEquals(0,record1.compareTo(recordList.getRecords().get(1)));
+        assertEquals(0,mockRecord().compareTo(recordList.getRecords().get(0)));
+        assertEquals(0,record2.compareTo(recordList.getRecords().get(2)));
+
+    }
+    @Test
+    public void testcount(){
+        /*creating and initializing objects for citylist and city class*/
+        RecordList recordList = new RecordList();
+        Record record1 = new Record("190","60","high","50","exceptional","Thursday,7 July 2022","05:49 pm","resting");
+        recordList.addRecords(record1);
+        Record record2 = new Record("90","60","high","50","exceptional","Thursday,7 July 2022","05:49 pm","resting");
+        recordList.addRecords(record2);
+        assertEquals(2,recordList.count());
+        recordList.delete(record1);
+        assertEquals(1,recordList.count());
+        recordList.delete(record2);
+    }
+
 
 
 
